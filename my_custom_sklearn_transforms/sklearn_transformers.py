43d91c59c94db967ddd9943ac964a3852fec8942
steve2ag <46571670+steve2ag@users.noreply.github.com>
2020-12-05 12:48:19 -0600

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,16 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+    
+class my_Encoder(BaseEstimator, TransformerMixin):
+    def __init__(self, columns = 'koi_pdisposition'):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        data = X.copy()
+        wt = {'CANDIDATE':1, 'FALSE POSITIVE':2}
+        data['koi_pdisposition'] = data['koi_pdisposition'].map(wt)
+        return data.drop(self.columns, axis='columns')
